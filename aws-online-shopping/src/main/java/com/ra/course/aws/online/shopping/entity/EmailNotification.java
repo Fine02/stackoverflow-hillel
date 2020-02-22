2f29494eca8a8ba21973e20d335ca40163cfb293
Halyna Tesliuk <tesliuk.ua@gmail.com>
2020-02-22 19:46:29 +0200

    ME-10 created fields, models and relation for classes

--- aws-online-shopping/src/main/java/com/ra/course/aws/online/shopping/entity/EmailNotification.java
+++ aws-online-shopping/src/main/java/com/ra/course/aws/online/shopping/entity/EmailNotification.java
@@ -5,6 +5,11 @@ import java.util.Date;
 public class EmailNotification extends  Notification {
     private String email;
 
+    public EmailNotification(int notificationId, Date createdOn, String content, String email) {
+        super(notificationId, createdOn, content);
+        this.email = email;
+    }
+
     public EmailNotification(int notificationId, Date createdOn, String content) {
         super(notificationId, createdOn, content);
     }
--- aws-online-shopping/src/main/java/com/ra/course/aws/online/shopping/entity/SMSNotification.java
+++ aws-online-shopping/src/main/java/com/ra/course/aws/online/shopping/entity/SMSNotification.java
@@ -5,6 +5,11 @@ import java.util.Date;
 public class SMSNotification extends Notification {
     private String phone;
 
+    public SMSNotification(int notificationId, Date createdOn, String content, String phone) {
+        super(notificationId, createdOn, content);
+        this.phone = phone;
+    }
+
     public SMSNotification(int notificationId, Date createdOn, String content) {
         super(notificationId, createdOn, content);
     }
