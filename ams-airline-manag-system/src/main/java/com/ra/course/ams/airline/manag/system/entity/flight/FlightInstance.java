3a98eb5f29dfade07517e8c42c7bd29e64ec61c2
Sergii Kukhar <sergiikukhar@gmail.com>
2020-02-24 22:39:06 +0200

    Add WeeklySchedule, CustomSchedule, Airline, Aircraft, Seat, Flight, Airport, FlightInstance, Flight Seat classes

---
+++ ams-airline-manag-system/src/main/java/com/ra/course/ams/airline/manag/system/entity/flight/FlightInstance.java
@@ -0,0 +1,116 @@
+package src.main.java.com.ra.course.ams.airline.manag.system.entity.flight;
+
+import java.sql.Time;
+import java.util.List;
+
+public class FlightInstance {
+
+    private Time departureTime;
+    private String gate;
+    private FlightStatus status;
+    private List<FlightSeat> seats;
+    private Aircraft aircraft;
+    // TODO resolve dependency
+    private List<Crew> crews;
+    // TODO resolve dependency
+    private List<Pilot> pilots;
+
+    // TODO implementation is not clear
+    private boolean cancel() {
+        return false;
+    }
+
+    private void updateStatus(FlightStatus status) {
+        this.status = status;
+    }
+
+    public FlightInstance() {
+    }
+
+    public FlightInstance(Time departureTime, String gate, FlightStatus status, List<FlightSeat> seats, Aircraft aircraft, List<Crew> crews, List<Pilot> pilots) {
+        this.departureTime = departureTime;
+        this.gate = gate;
+        this.status = status;
+        this.seats = seats;
+        this.aircraft = aircraft;
+        this.crews = crews;
+        this.pilots = pilots;
+    }
+
+    public Time getDepartureTime() {
+        return departureTime;
+    }
+
+    public FlightInstance setDepartureTime(Time departureTime) {
+        this.departureTime = departureTime;
+        return this;
+    }
+
+    public String getGate() {
+        return gate;
+    }
+
+    public FlightInstance setGate(String gate) {
+        this.gate = gate;
+        return this;
+    }
+
+    public FlightStatus getStatus() {
+        return status;
+    }
+
+    public FlightInstance setStatus(FlightStatus status) {
+        this.status = status;
+        return this;
+    }
+
+    public List<FlightSeat> getSeats() {
+        return seats;
+    }
+
+    public FlightInstance setSeats(List<FlightSeat> seats) {
+        this.seats = seats;
+        return this;
+    }
+
+    public Aircraft getAircraft() {
+        return aircraft;
+    }
+
+    public FlightInstance setAircraft(Aircraft aircraft) {
+        this.aircraft = aircraft;
+        return this;
+    }
+
+    public List<Crew> getCrews() {
+        return crews;
+    }
+
+    public FlightInstance setCrews(List<Crew> crews) {
+        this.crews = crews;
+        return this;
+    }
+
+    public List<Pilot> getPilots() {
+        return pilots;
+    }
+
+    public FlightInstance setPilots(List<Pilot> pilots) {
+        this.pilots = pilots;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("src.main.java.com.ra.course.ams.airline.manag.system.entity.flight.FlightInstance{");
+        sb.append("\"departureTime\": \"").append(departureTime).append('"');
+        sb.append(", \"gate\": \"").append(gate).append('"');
+        sb.append(", \"status\":").append(status);
+        sb.append(", \"seats\":").append(seats);
+        sb.append(", \"aircraft\":").append(aircraft);
+        sb.append(", \"crews\":").append(crews);
+        sb.append(", \"pilots\":").append(pilots);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ ams-airline-manag-system/src/main/java/com/ra/course/ams/airline/manag/system/entity/flight/Seat.java
@@ -0,0 +1,89 @@
+package src.main.java.com.ra.course.ams.airline.manag.system.entity.flight;
+
+public class Seat {
+
+    private String seatNumber;
+    private SeatType type;
+    // set name of field seatClass because of 'class' is key word
+    private SeatClass seatClass;
+
+    public Seat() {
+    }
+
+    public Seat(String seatNumber, SeatType type, SeatClass seatClass) {
+        this.seatNumber = seatNumber;
+        this.type = type;
+        this.seatClass = seatClass;
+    }
+
+    private Seat(Builder builder) {
+        seatNumber = builder.seatNumber;
+        type = builder.type;
+        seatClass = builder.seatClass;
+    }
+
+    public static class Builder {
+
+        private String seatNumber;
+        private SeatType type;
+        private SeatClass seatClass;
+
+        public Builder () {}
+
+        public Builder setSeatNumber(String seatNumber) {
+            this.seatNumber = seatNumber;
+            return this;
+        }
+
+        public Builder setType(SeatType type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder setSeatClass(SeatClass seatClass) {
+            this.seatClass = seatClass;
+            return this;
+        }
+
+        public Seat build() {
+            return new Seat(this);
+        }
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public Seat setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+        return this;
+    }
+
+    public SeatType getType() {
+        return type;
+    }
+
+    public Seat setType(SeatType type) {
+        this.type = type;
+        return this;
+    }
+
+    public SeatClass getSeatClass() {
+        return seatClass;
+    }
+
+    public Seat setSeatClass(SeatClass seatClass) {
+        this.seatClass = seatClass;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("src.main.java.com.ra.course.ams.airline.manag.system.entity.flight.Seat{");
+        sb.append("\"seatNumber\": \"").append(seatNumber).append('"');
+        sb.append(", \"seatType\":").append(type);
+        sb.append(", \"seatClass\":").append(seatClass);
+        sb.append('}');
+        return sb.toString();
+    }
+}
