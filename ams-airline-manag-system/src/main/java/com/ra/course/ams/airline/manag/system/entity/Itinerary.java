7f9bc6f319282a94b73030767ebd419361d6302e
Sergii Kukhar <sergiikukhar@gmail.com>
2020-02-25 09:58:04 +0200

    Fix startingAirport and finalAirport fields in Itenarary class

--- ams-airline-manag-system/src/main/java/com/ra/course/ams/airline/manag/system/entity/Itinerary.java
+++ ams-airline-manag-system/src/main/java/com/ra/course/ams/airline/manag/system/entity/Itinerary.java
@@ -1,15 +1,37 @@
 package com.ra.course.ams.airline.manag.system.entity;
 
+import com.ra.course.ams.airline.manag.system.entity.flight.Airport;
+
 import java.util.Date;
 import java.util.List;
 
 public class Itinerary {
 
+    private Airport startingAirport;
+    private Airport finalAirport;
     private Date creationDate;
     private List<Passenger> passengers;
     private List<FlightReservation> flightReservations;
 
     public Itinerary() {
+    }
+
+    public Airport getStartingAirport() {
+        return startingAirport;
+    }
+
+    public Itinerary setStartingAirport(Airport startingAirport) {
+        this.startingAirport = startingAirport;
+        return this;
+    }
+
+    public Airport getFinalAirport() {
+        return finalAirport;
+    }
+
+    public Itinerary setFinalAirport(Airport finalAirport) {
+        this.finalAirport = finalAirport;
+        return this;
     }
 
     public Itinerary(Date creationDate) {
