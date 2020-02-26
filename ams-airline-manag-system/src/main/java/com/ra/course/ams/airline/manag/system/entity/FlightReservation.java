bd4b804efca57053e835d22144aa976e7c688a0a
tyshenko.roma <tyshenko.roma@gmail.com>
2020-02-26 20:55:28 +0200

    resolved some comments

--- ams-airline-manag-system/src/main/java/com/ra/course/ams/airline/manag/system/entity/FlightReservation.java
+++ ams-airline-manag-system/src/main/java/com/ra/course/ams/airline/manag/system/entity/FlightReservation.java
@@ -2,20 +2,54 @@ package com.ra.course.ams.airline.manag.system.entity;
 
 import com.ra.course.ams.airline.manag.system.entity.flight.FlightInstance;
 import com.ra.course.ams.airline.manag.system.entity.flight.FlightSeat;
+import com.ra.course.ams.airline.manag.system.entity.notification.Notification;
+import com.ra.course.ams.airline.manag.system.entity.payment.Payment;
+import com.ra.course.ams.airline.manag.system.entity.person.Pilot;
 
-import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 
 public class FlightReservation {
     private String reservationNumber;
     private FlightInstance flight;
-    private Map<Passenger, FlightSeat> seatMap;
+    private HashMap <Passenger, FlightSeat> seatMap;
     private ReservationStatus status;
+    private List<Notification> notifications;
+    private Payment payment;
+    private List<Passenger> passengers;
 
-    public FlightReservation(String reservationNumber, FlightInstance flight, Map<Passenger, FlightSeat> seatMap, ReservationStatus status) {
+
+    public FlightReservation(String reservationNumber, FlightInstance flight, HashMap<Passenger, FlightSeat> seatMap, ReservationStatus status, List<Notification> notifications, Payment payment) {
         this.reservationNumber = reservationNumber;
         this.flight = flight;
         this.seatMap = seatMap;
         this.status = status;
+        this.notifications = notifications;
+        this.payment = payment;
+    }
+
+    public List<Notification> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(List<Notification> notifications) {
+        this.notifications = notifications;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(List<Passenger> passengers) {
+        this.passengers = passengers;
     }
 
     public String getReservationNumber() {
@@ -34,11 +68,11 @@ public class FlightReservation {
         this.flight = flight;
     }
 
-    public Map<Passenger, FlightSeat> getSeatMap() {
+    public HashMap<Passenger, FlightSeat> getSeatMap() {
         return seatMap;
     }
 
-    public void setSeatMap(Map<Passenger, FlightSeat> seatMap) {
+    public void setSeatMap(HashMap<Passenger, FlightSeat> seatMap) {
         this.seatMap = seatMap;
     }
 
