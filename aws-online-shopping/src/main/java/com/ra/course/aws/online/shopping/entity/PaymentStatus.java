f392052d153ac7bc4f93ef55a0a58ed47d269c37
tyshenko.roma <tyshenko.roma@gmail.com>
2020-02-26 22:21:59 +0200

    deleted ams-online-shopping from gradle.settings

--- aws-online-shopping/src/main/java/com/ra/course/aws/online/shopping/entity/PaymentStatus.java
+++
@@ -1,5 +0,0 @@
-package com.ra.course.aws.online.shopping.entity;
-
-public enum PaymentStatus {
-    UNPAID,PENDING,COMPLETED,FAILD,DECLINED,CANCELED,ABANDONED,SETTLING,SETTLED,REFUNDER
-}
--- aws-online-shopping/src/main/java/com/ra/course/aws/online/shopping/entity/ShipmentStatus.java
+++
@@ -1,5 +0,0 @@
-package com.ra.course.aws.online.shopping.entity;
-
-public enum ShipmentStatus {
-    PENDING, SHIPPED, DELIVERED, ONHOLD
-}
