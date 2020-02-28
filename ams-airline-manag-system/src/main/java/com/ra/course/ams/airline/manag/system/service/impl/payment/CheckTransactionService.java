bdc6043b5eaa1bcdc6686c7ddd5a911cfa4a57a4
yshenko.roma <Tyshenko.roma@gmail.com>
2020-02-28 13:52:41 +0200

    finish first check

--- ams-airline-manag-system/src/main/java/com/ra/course/ams/airline/manag/system/service/impl/payment/CheckTransactionService.java
+++ ams-airline-manag-system/src/main/java/com/ra/course/ams/airline/manag/system/service/impl/payment/CheckTransactionService.java
@@ -1,6 +1,7 @@
 package com.ra.course.ams.airline.manag.system.service.impl.payment;
 
 import com.ra.course.ams.airline.manag.system.entity.payment.CheckTransaction;
+import com.ra.course.ams.airline.manag.system.entity.payment.PaymentStatus;
 import com.ra.course.ams.airline.manag.system.service.PaymentService;
 
 public class CheckTransactionService implements PaymentService <CheckTransaction> {
@@ -9,9 +10,12 @@ public class CheckTransactionService implements PaymentService <CheckTransaction
     public boolean makeTransaction(CheckTransaction payment) {
         if (payment.getPaimentId() == 0 || payment.getBankName().isBlank()  ||
             payment.getCheckNumber().isBlank() || payment.getAmound() == 0) {
-
+            payment.setStaus(PaymentStatus.FAILED);
+            throw  new IllegalArgumentException("Payment fields can't de empty");
+        } else {
+            payment.setStaus(PaymentStatus.COMPLETED);
         }
-        return false;
+        return true;
     }
 
     @Override
