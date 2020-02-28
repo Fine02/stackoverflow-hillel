db0b2200f7d74a747908e814155978716b41eba8
yshenko.roma <Tyshenko.roma@gmail.com>
2020-02-28 13:42:45 +0200

    add first check

--- ams-airline-manag-system/src/main/java/com/ra/course/ams/airline/manag/system/service/impl/payment/CheckTransactionService.java
+++ ams-airline-manag-system/src/main/java/com/ra/course/ams/airline/manag/system/service/impl/payment/CheckTransactionService.java
@@ -1,17 +1,21 @@
 package com.ra.course.ams.airline.manag.system.service.impl.payment;
 
-import com.ra.course.ams.airline.manag.system.entity.payment.Payment;
+import com.ra.course.ams.airline.manag.system.entity.payment.CheckTransaction;
 import com.ra.course.ams.airline.manag.system.service.PaymentService;
 
-public class CheckTransactionService implements PaymentService {
+public class CheckTransactionService implements PaymentService <CheckTransaction> {
 
     @Override
-    public boolean makeTransaction(final Payment payment) {
+    public boolean makeTransaction(CheckTransaction payment) {
+        if (payment.getPaimentId() == 0 || payment.getBankName().isBlank()  ||
+            payment.getCheckNumber().isBlank() || payment.getAmound() == 0) {
+
+        }
         return false;
     }
 
     @Override
-    public boolean refundPayment(final Payment payment) {
+    public boolean refundPayment(CheckTransaction payment) {
         return false;
     }
 }
