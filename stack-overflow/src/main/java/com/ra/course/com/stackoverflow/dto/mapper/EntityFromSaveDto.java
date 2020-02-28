7993c2f847a62f95f7fb125c9dc3b90935840e9e
Alina <alinna135@gmail.com>
2020-02-28 21:44:47 +0200

    [SO] del unused file

--- stack-overflow/src/main/java/com/ra/course/com/stackoverflow/dto/mapper/EntityFromSaveDto.java
+++ stack-overflow/src/main/java/com/ra/course/com/stackoverflow/dto/mapper/EntityFromSaveDto.java
@@ -1,7 +1,6 @@
 package com.ra.course.com.stackoverflow.dto.mapper;
 
 import com.ra.course.com.stackoverflow.dto.GeneralSaveDto;
-import com.ra.course.com.stackoverflow.entity.interfaces.IDEntity;
 import org.mapstruct.Mapper;
 
 @Mapper
--- stack-overflow/src/main/java/com/ra/course/com/stackoverflow/entity/interfaces/IDEntity.java
+++
@@ -1,6 +0,0 @@
-package com.ra.course.com.stackoverflow.entity.interfaces;
-
-public interface IDEntity {
-
-    long getId();
-}
