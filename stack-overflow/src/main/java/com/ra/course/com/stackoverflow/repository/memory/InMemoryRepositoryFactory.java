93a0391cf1365d8bbff4966f142e426d870d6173
Alina <alinna135@gmail.com>
2020-02-28 10:26:34 +0200

    [SO] updated impl of repositoryFactory

--- stack-overflow/src/main/java/com/ra/course/com/stackoverflow/repository/memory/InMemoryRepositoryFactory.java
+++ stack-overflow/src/main/java/com/ra/course/com/stackoverflow/repository/memory/InMemoryRepositoryFactory.java
@@ -3,23 +3,28 @@ package com.ra.course.com.stackoverflow.repository.memory;
 import com.ra.course.com.stackoverflow.repository.interfaces.*;
 
 public class InMemoryRepositoryFactory implements RepositoryFactory {
+    private final transient InMemoryAnswerRepository answerRepo = new InMemoryAnswerRepository();
+    private final transient InMemoryQuestionRepository questionRepo = new InMemoryQuestionRepository();
+    private final transient InMemoryMemberRepository memberRepo = new InMemoryMemberRepository();
+    private final transient InMemoryTagRepository tagRepo = new InMemoryTagRepository();
+
     @Override
     public AnswerRepository createAnswerRepository() {
-        return new InMemoryAnswerRepository();
+        return answerRepo;
     }
 
     @Override
     public QuestionRepository createQuestionRepository() {
-        return new InMemoryQuestionRepository();
+        return questionRepo;
     }
 
     @Override
     public MemberRepository createMemberRepository() {
-        return new InMemoryMemberRepository();
+        return memberRepo;
     }
 
     @Override
     public TagRepository createTagRepository() {
-        return new InMemoryTagRepository();
+        return tagRepo;
     }
 }
