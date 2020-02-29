609e0864e392d67fdbc1731207a6149045e9ccd7
Alina <alinna135@gmail.com>
2020-02-29 16:38:46 +0200

    [SO] added exceptions

---
+++ stack-overflow/src/main/java/com/ra/course/com/stackoverflow/exception/repository/AnswerNotFoundException.java
@@ -0,0 +1,15 @@
+package com.ra.course.com.stackoverflow.exception.repository;
+
+
+public class AnswerNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public AnswerNotFoundException(final String message) {
+        super(message);
+    }
+
+    public AnswerNotFoundException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
