7f296b01c678e8912f37af18bfef53ab6cff1d1a
ShadowsDream <fedorovpavlo@gmail.com>
2020-03-11 22:16:03 +0200

    [SO] refactored services, tests and deleted Commentable

--- stack-overflow/src/main/java/com/ra/course/com/stackoverflow/service/member/MemberServiceImpl.java
+++ stack-overflow/src/main/java/com/ra/course/com/stackoverflow/service/member/MemberServiceImpl.java
@@ -2,64 +2,30 @@ package com.ra.course.com.stackoverflow.service.member;
 
 import com.ra.course.com.stackoverflow.entity.Member;
 import com.ra.course.com.stackoverflow.entity.Question;
-import com.ra.course.com.stackoverflow.exception.repository.DataBaseOperationException;
-import com.ra.course.com.stackoverflow.exception.service.InternalServerErrorException;
 import com.ra.course.com.stackoverflow.repository.interfaces.MemberRepository;
 import com.ra.course.com.stackoverflow.repository.interfaces.QuestionRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
-import java.util.Objects;
-
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService<Question> {
 
+    @NonNull
     private transient final QuestionRepository questionRep;
+
+    @NonNull
     private transient final MemberRepository memberRep;
 
-    private static final String SERVER_ERR_MSG = "Unexpected data base error occurred: ";
-    private static final String NULL_WARNING_MSG = "argument must not be null";
-
-    public MemberServiceImpl(final QuestionRepository questionRep,
-                             final MemberRepository memberRep) {
-        Objects.requireNonNull(questionRep, "questionRep' " + NULL_WARNING_MSG);
-        Objects.requireNonNull(memberRep, "'memberRep' " + NULL_WARNING_MSG);
-
-        this.questionRep = questionRep;
-        this.memberRep = memberRep;
-    }
-
-
     @Override
-    public Question postQuestion(final Question question) {
-        Objects.requireNonNull(question, "'question' " + NULL_WARNING_MSG);
-
-        final Question postedQuestion = saveQuestion(question);
-        updateMember(postedQuestion);
-
-        return postedQuestion;
-    }
-
-    private Question saveQuestion(final Question question) {
-
-        try {
-            return questionRep.save(question);
-        } catch (final DataBaseOperationException e) {
-            throw (InternalServerErrorException)
-                    new InternalServerErrorException(SERVER_ERR_MSG + e.getMessage()).initCause(e);
-        }
-    }
-
-    private Member updateMember(final Question question) {
+    public Question postQuestion(@NonNull final Question question) {
+        final Question postedQuestion = questionRep.save(question);
 
         final Member member = question.getAuthor();
 
-        member.getQuestions().add(question);
+        member.getQuestions().add(postedQuestion);
 
-        try {
-            memberRep.update(member);
-        } catch (final DataBaseOperationException e) {
-            throw (InternalServerErrorException)
-                    new InternalServerErrorException(SERVER_ERR_MSG + e.getMessage()).initCause(e);
-        }
+        memberRep.update(member);
 
-        return member;
+        return postedQuestion;
     }
 }
