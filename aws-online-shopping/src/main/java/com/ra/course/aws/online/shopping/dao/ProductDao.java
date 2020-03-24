b664c490a4f11f909acbac14a3b2f352415cd47a
Halyna Tesliuk <tesliuk.ua@gmail.com>
2020-03-24 13:28:06 +0200

    deleted unused annotation

--- aws-online-shopping/src/main/java/com/ra/course/aws/online/shopping/dao/ProductDao.java
+++ aws-online-shopping/src/main/java/com/ra/course/aws/online/shopping/dao/ProductDao.java
@@ -1,11 +1,9 @@
 package com.ra.course.aws.online.shopping.dao;
 
 import com.ra.course.aws.online.shopping.entity.product.Product;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface ProductDao {
 
     Long save(Product product);
--- aws-online-shopping/src/main/java/com/ra/course/aws/online/shopping/dao/ShoppingCartDao.java
+++ aws-online-shopping/src/main/java/com/ra/course/aws/online/shopping/dao/ShoppingCartDao.java
@@ -2,11 +2,9 @@ package com.ra.course.aws.online.shopping.dao;
 
 import com.ra.course.aws.online.shopping.entity.Item;
 import com.ra.course.aws.online.shopping.entity.enums.PaymentStatus;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface ShoppingCartDao {
     Long addItemToCart(Item item);
     Item getItemFromCart(Long productId);
