2b83f34bd7a944312ca542a740fcb67bf0abc471
Chernenko Vitalii <chernenko_vitaliy@ukr.net>
2020-04-28 00:47:05 +0300

    rewritten CommentServiceIntegrationTest and QuestionServiceIntegrationTest by comment in Pull Request

--- stack-overflow-integration-test/src/test/java/com/ra/course/com/stackoverflow/service/QuestionServiceIntegrationTest.java
+++ stack-overflow-integration-test/src/test/java/com/ra/course/com/stackoverflow/service/QuestionServiceIntegrationTest.java
@@ -1,64 +1,124 @@
 package com.ra.course.com.stackoverflow.service;
 
-import com.ra.course.com.stackoverflow.entity.Question;
-import com.ra.course.com.stackoverflow.entity.Tag;
+import com.ra.course.com.stackoverflow.entity.*;
+import com.ra.course.com.stackoverflow.entity.enums.QuestionStatus;
+import com.ra.course.com.stackoverflow.exception.service.QuestionClosedException;
 import com.ra.course.com.stackoverflow.exception.service.TagAlreadyAddedException;
-import com.ra.course.com.stackoverflow.repository.QuestionRepository;
-import com.ra.course.com.stackoverflow.repository.TagRepository;
 import com.ra.course.com.stackoverflow.service.question.QuestionService;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Sql({"classpath:schema.sql", "classpath:data.sql"})
 public class QuestionServiceIntegrationTest {
+    private final long ID = 1L;
+    private Account account = createNewAccount(ID);
+    private Member member = createNewMember(account);
+    private Question openQuestion = createNewQuestion(ID, member);
+    private Question closedQuestion = createNewQuestion(2L, member);
+    private Answer answer = createNewAnswer(ID, member, openQuestion);
+    private Tag tag = createNewTag(ID);
 
     @Autowired
     private QuestionService questionService;
-    @Autowired
-    private TagRepository tagRepository;
-    @Autowired
-    private QuestionRepository questionRepository;
-    private Question question;
-    private Tag tag;
+
 
     @Test
-    @DisplayName("Integration test for QuestionService to add tag to question then OK")
-    public void whenAddTagToQuestionThenReturnTrue() {
-        injectQuestionAndTagPair(1L, "SQL");
+    public void whenAddAnswerToOpenQuestionThenQuestionContainsAnswerInList() {
+        //given
+        openQuestion.setStatus(QuestionStatus.OPEN);
 
-        assertTrue(questionService.addTagToQuestion(tag, question));
+        //when
+        var result = questionService.addAnswerToQuestion(answer);
+
+        //then
+        assertThat(result.getAnswerList()).contains(answer);
     }
 
     @Test
-    @DisplayName("Integration test for QuestionService to add tag to question then OK")
-    public void ifTagNotFoundInDBShouldSaveTagInDB() {
-        question = questionRepository.findById(1L).get();
-        tag = new Tag(999L,"HTML", "For frond-end developers ONLY");
+    public void whenAddAnswerToClosedQuestionThenThrowException() {
+        //given
+        var answer = createNewAnswer(1L, member, closedQuestion);
 
-        assertTrue(questionService.addTagToQuestion(tag, question));
-        assertThat(question.getTagList().contains(tag));
+        //then
+        assertThatThrownBy(() -> questionService.addAnswerToQuestion(answer))
+                .isInstanceOf(QuestionClosedException.class);
     }
 
     @Test
-    @DisplayName("Integration test for QuestionService to add tag to question then throws exception")
-    public void ifTagAlreadyAddedToQuestionThrowException() {
-        injectQuestionAndTagPair(2L, "JAVA");
+    public void whenAddTagToOpenQuestionThenReturnTrue() {
+        //given
+        openQuestion.setStatus(QuestionStatus.OPEN);
 
-        assertThatThrownBy(() -> questionService.addTagToQuestion(tag, question))
+        //when
+        var result = questionService.addTagToQuestion(tag, openQuestion);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenAddTagToOpenQuestionButTagAlreadyAddedThenThrowException() {
+        //given
+        openQuestion.setStatus(QuestionStatus.OPEN);
+
+        //when
+        var result = questionService.addTagToQuestion(tag, openQuestion);
+
+        //then
+        assertThatThrownBy(() -> questionService.addTagToQuestion(tag, openQuestion))
                 .isInstanceOf(TagAlreadyAddedException.class);
     }
 
-    private void injectQuestionAndTagPair(long commentId, String tagName) {
-        question = questionRepository.findById(commentId).get();
-        tag = tagRepository.findByTagName(tagName).get();
+
+    private Account createNewAccount(long id) {
+        return Account.builder()
+                .id(id)
+                .password("password")
+                .email("email")
+                .name("name")
+                .build();
     }
 
+    private Member createNewMember(Account account) {
+        return Member.builder()
+                .account(account)
+                .build();
+    }
+
+    private Question createNewQuestion(long id, Member member) {
+        return Question.builder()
+                .id(id)
+                .description("some_question")
+                .title("title")
+                .authorId(member.getAccount().getId())
+                .build();
+    }
+
+    private Answer createNewAnswer(long id, Member member, Question question) {
+        return Answer.builder()
+                .id(id)
+                .answerText("answer_text")
+                .creationDate(LocalDateTime.now())
+                .authorId(member.getAccount().getId())
+                .questionId(question.getId())
+                .photos(new ArrayList<>())
+                .comments(new ArrayList<>())
+                .build();
+    }
+
+    private Tag createNewTag(long id) {
+        return new Tag(id, "tag_name", "tag_description");
+    }
 }
